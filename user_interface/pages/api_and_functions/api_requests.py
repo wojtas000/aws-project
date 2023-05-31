eb25a00d1f9a6148baaf837af738b29dbab1bfde
wojtas000 <jan2000@op.pl>
2023-05-31 15:44:24 +0200

    update api

--- user_interface/pages/api_and_functions/api_requests.py
+++ user_interface/pages/api_and_functions/api_requests.py
@@ -1,16 +1,22 @@
 import requests
 import base64
+import json
+import base64
+import io
+
+API_ROOT = 'https://1tn5vi9uuh.execute-api.eu-central-1.amazonaws.com/prod'
 
 LIST_IMAGES_ENDPOINT = "https://1tn5vi9uuh.execute-api.eu-central-1.amazonaws.com/prod/list-images"
 USER_HISTORY_ENDPOINT = "https://1tn5vi9uuh.execute-api.eu-central-1.amazonaws.com/prod/user-history"
 INSERT_WATERMARK_ENDPOINT = "https://1tn5vi9uuh.execute-api.eu-central-1.amazonaws.com/prod/add-watermark"
 
+
 def get_image_list(username, bucket_type):
-    payload = {
+    params = {
     "username": username,
     "bucket_type": bucket_type
         }
-    response = requests.get(LIST_IMAGES_ENDPOINT, json=payload)
+    response = requests.get(API_ROOT + f'/user/{username}/images', json=params)
 
     if response.status_code == 200:
         
@@ -18,7 +24,7 @@ def get_image_list(username, bucket_type):
 
         return eval(response_json['body'])
     else:
-        return []
+        return response.status_code
     
 
 def get_user_history(username):
@@ -26,7 +32,7 @@ def get_user_history(username):
                 "user_login": username
                }
     try:
-        response = requests.get(USER_HISTORY_ENDPOINT, json=payload)
+        response = requests.get(API_ROOT + f'/user/{username}/history', json=payload)
     except:
         return {}
 
@@ -39,12 +45,6 @@ def get_user_history(username):
     else:
         return {}
     
-
-import requests
-import json
-
-# Define the input parameters for the Lambda function
-import base64
 
 def add_watermark(main_image_bytes, watermark_image_bytes, X, Y):
     payload = {
@@ -59,12 +59,13 @@ def add_watermark(main_image_bytes, watermark_image_bytes, X, Y):
 
     # Make the POST request to invoke the Lambda function
     try:
-        response = requests.post(INSERT_WATERMARK_ENDPOINT, data=payload_json)
+        response = requests.post(API_ROOT + '/images/watermark', data=payload_json)
     except:
         return False
 
     # Check the response status code
     if response.status_code == 200:
+
         response_json = response.json()
         response_body = eval(response_json['body'])['result_image']
         # result_image_base64 = response_body[9:]
@@ -73,3 +74,24 @@ def add_watermark(main_image_bytes, watermark_image_bytes, X, Y):
     
     else:
         return False
+
+
+def upload_image(image_data, image_name, bucket_type, username):
+    endpoint = API_ROOT + f'/user/{username}/{bucket_type}/images'
+    payload = {
+        'body': base64.b64encode(image_data).decode('utf-8'),
+        'name': image_name,
+        'bucket_type': bucket_type,
+        'username': username
+    }
+    print(image_name, bucket_type, username)
+    print(endpoint)
+    try:
+        response = requests.post(endpoint, json=payload)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f'Failed to upload image. Status code: {response.status_code}. Response: {response.text}')
+    except requests.exceptions.RequestException as e:
+        print('Error connecting to the API:', e)
+
